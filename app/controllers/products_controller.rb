2b5753b7a471a38bae2a70697d51cbfe05ec1417
ribrciti <rbciti@msn.com>
2015-11-16 10:47:01 -0800

    Added images to seed products and created show view with minimal styling

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,55 @@
 class ProductsController < ApplicationController
 
+	before_action :find_product, only: [:show, :edit, :update, :destroy]
+
 	def index
+		@products = Product.all.order("created_at DESC")
+	end
+
+	def new
+		@product = Product.new
+		@product.id = Product.maximum(:id)+1
 		
 	end
+
+	def create
+		@product = Product.new(product_params)
+
+		if @product.save
+			redirect_to @product, notice: "Your Product was successfully saved!"
+		else
+			render 'new', notice: "Your article was unable to be saved!"
+		end
+	end
+
+	def show
+	end
+
+	def edit		
+	end
+
+	def update
+		if @product.update(product_params)
+			redirect_to @product, notice: "Your udpdate was successful!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@product.destroy
+		redirect_to products_path
+	end
+
+	private
+
+	def find_product
+		@product = Product.find(params[:id])
+	end
+
+	def product_params
+		params.require(:product).permit(:name, :sku, :description, :short_description,
+		 :permalink, :weight, :price, :cost_price, :category_name)
+	end
+
 end
---
+++ lib/tasks/populate.rake
@@ -0,0 +1,19 @@
+namespace :db do
+	desc "Erase and fill database"
+	task :populate => :environment do
+		require 'populator'
+		require 'faker'
+
+		[Product].each(&:delete_all)
+
+		Product.populate 15 do | product |
+			product.name = Populator.words(1..3).titleize
+			product.sku = rand(00000..99999)
+			product.permalink = product.name
+			product.description = Populator.sentences(2..10)
+			product.short_description = product.description.first(99)
+			product.price = rand(23.99..129.99)
+			product.created_at = 1.years.ago..Time.now
+		end
+	end
+end
