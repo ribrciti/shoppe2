c059626a01e733db1d04df86765cb624c096f879
ribrciti <rbciti@msn.com>
2015-11-28 13:40:58 -0800

    Added pagination to store view

--- Gemfile
+++ Gemfile
@@ -17,3 +17,4 @@ gem 'faker', '~> 1.5'
 gem 'foundation-rails', '~> 5.5', '>= 5.5.3.2'
 gem 'pry', '~> 0.10.3', group: :development 
 gem 'pry-byebug', '~> 3.3', group: :development 
+gem 'will_paginate', '~> 3.0', '>= 3.0.7'
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,8 @@
 class StoreController < ApplicationController
 
   def index
-  	@products = Product.all.order(:name).limit(4)
+  	@products = Product.all.order(:id).paginate(page: params[:page], per_page: 3)	
+  	#@products = Product.all.order(:name).limit(4)
   	@cart = current_cart
   end
 
